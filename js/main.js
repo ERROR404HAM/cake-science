f543285945eeebec416f2983a146f913a4165e71
YOU BROOOOOOOOO.... <244197890+ERROR404HAM@users.noreply.github.com>
2026-04-20 16:54:22 +0530

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// frontend logic
-
-// This is a sample frontend logic for the cake-science project
-
-function displayWelcomeMessage() {
-    console.log('Welcome to Cake Science!');
-}
-
-displayWelcomeMessage();
