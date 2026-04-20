4b2993779202907ad651eb2a20427914d06dc2fd
YOU BROOOOOOOOO.... <244197890+ERROR404HAM@users.noreply.github.com>
2026-04-20 16:48:29 +0530

    Create js/main.js file with frontend logic.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// frontend logic
+
+// This is a sample frontend logic for the cake-science project
+
+function displayWelcomeMessage() {
+    console.log('Welcome to Cake Science!');
+}
+
+displayWelcomeMessage();
